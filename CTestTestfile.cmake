dff3f039ffba41a4c51210c01b7de88f9db68a67
Garrett Van Mourik <gvanmou@s1017129ca.lan>
2018-01-17 10:15:54 -0800

    after .gitignore redo

--- CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/gvanmou/Desktop/workflowProject
-# Build directory: /Users/gvanmou/Desktop/workflowProject
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("pintools")
--- pintools/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/gvanmou/Desktop/workflowProject/pintools
-# Build directory: /Users/gvanmou/Desktop/workflowProject/pintools
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
